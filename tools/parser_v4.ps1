65173853575082c6084c9340aafb6a5ef7e2cb08
Andoni Muruamendiaraz <andoni.muruamendiaraz@globetesting.com>
2017-10-23 16:58:17 +0200

    JUnit structure fixed and comments added

--- tools/parser_v4.ps1
+++ tools/parser_v4.ps1
@@ -1,63 +1,66 @@
-﻿#parser resultados UFT xml a formato junit
+﻿###########################################################
+#     Parse UFT run_results.xml to a JUnit format XML     #
+###########################################################
 
-$total_duration =0;
-$total_name ="NAME";
-$total_failures =0;
-
-$ruta_xml_origen = $args[0]
+# Get external parameters
+$origin_xml_directory = $args[0]
 $ruta_xml_junit = $args[1]
-#$ruta_xml_origen = "C:\Users\globe\Box Sync\Andoni Muruamendiaraz\Proyectos\CONSULTING\XML\go"
+$func_name = $args[2]
+#$origin_xml_directory = "C:\Users\globe\Box Sync\Andoni Muruamendiaraz\Proyectos\CONSULTING\XML\go"
 #$ruta_xml_junit = "C:\Users\globe\Box Sync\Andoni Muruamendiaraz\Proyectos\CONSULTING\reporte.xml"
 
-#Create a new XML File with config root node
-[System.XML.XMLDocument]$oXMLDocument=New-Object System.XML.XMLDocument
+# Init testsuite variables
+$total_duration = 0;
+$total_failures = 0;
 
-# Nuevo nodo "testsuite"
-[System.XML.XMLElement]$oXMLRoot=$oXMLDocument.CreateElement("testsuites")
+# Create a new XML File with config root node
+[System.XML.XMLDocument] $oXMLDocument = New-Object System.XML.XMLDocument
+
+# New node "testsuite"
+[System.XML.XMLElement] $oXMLRoot = $oXMLDocument.CreateElement("testsuite")
 
 # Append as child to an existing node
 $oXMLDocument.appendChild($oXMLRoot)
 
-Get-ChildItem $ruta_xml_origen |
-
+#Iterate throught all the files (testcases) on a directory
+Get-ChildItem $origin_xml_directory |
 Foreach-Object {
 	
-	#inicializacion
-	$pasado = 0
-	$fallado = 0
-	$passed = 0
-	$failed = 0
-	$ruta_xml_origen = $($_.FullName + "\Report\run_results.xml")
-	$TC_info_xpath = "/Results/ReportNode/Data/"
+	# Access to results file
+	$origin_xml_directory = $($_.FullName + "\Report\run_results.xml")
 
+	# Create new XML object
 	[System.Xml.XmlDocument]$file = new-object System.Xml.XmlDocument
 
-	#carga del fichero XML salida de UFT
-	$file.load($ruta_xml_origen)
+	# Load result file into XML object
+	$file.load($origin_xml_directory)
 
-	#ruta al nombre del script UFT
+	# Get the useful information of the results
 	$TC_name= $file.SelectSingleNode("/Results/ReportNode/Data/Name").innertext
 	$TC_duration= $file.SelectSingleNode("/Results/ReportNode/Data/Duration").innertext
 	$TC_result= $file.SelectSingleNode("/Results/ReportNode/Data/Result").innertext
 
-	[System.XML.XMLElement]$oXMLRootChild=$oXMLRoot.appendChild($oXMLDocument.CreateElement("testsuite"))
+	# Add testcase node
+	[System.XML.XMLElement]$oXMLRootChild=$oXMLRoot.appendChild($oXMLDocument.CreateElement("testcase"))
 
-	# Añadir atributos al testsuite: nombre, duracion, total steps
+	# Add previous info to the output JUnit XML
 	$oXMLRootChild.SetAttribute("name",$TC_name)        
-	$oXMLRootChild.SetAttribute("duration",$TC_duration)               
+	$oXMLRootChild.SetAttribute("time",$TC_duration)               
 	$oXMLRootChild.SetAttribute("failures",$TC_result)   
 
+	# Store the current TC info into variables to summary them into the functionality Testsuite
 	$total_duration = $total_duration + [int]$TC_duration
 	$total_failures = $total_failures + $(If ($TC_result -eq "Done") {0} Else {1}) 
 	
 }
 
-$oXMLRoot.SetAttribute("name",$total_name)        
-$oXMLRoot.SetAttribute("duration",$total_duration)               
+# Store the summarized info in the testsuite
+$oXMLRoot.SetAttribute("name",$func_name)        
+$oXMLRoot.SetAttribute("time",$total_duration)               
 $oXMLRoot.SetAttribute("failures",$total_failures) 
 
-# Save File
-$oXMLDocument.Save($ruta_xml_junit)
+# Save the output JUnit file
+$oXMLDocument.Save($($ruta_xml_junit + "\" + $func_name + "_junit_results.xml"))
 
 
 
